c12d0ac1c4629f6aab830d50163c512af620e644
andrusch <charles.h.andrus@gmail.com>
2014-04-12 10:48:32 -0500

    Ship update

--- Good-Ideas-Forever/Assets/Scripts/EnemyShip.cs
+++ Good-Ideas-Forever/Assets/Scripts/EnemyShip.cs
@@ -5,12 +5,28 @@ public class EnemyShip : Ship {
 
 	// Use this for initialization
 	void Start () {
-	
+		this.Health = 3;
+		this.Peace = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+	public NinjaForce Side 
+	{
+		get; 
+		set;
+	}
+	public int Health 
+	{
+		get;
+		set;
+	}
+	public int Peace 
+	{
+		get;
+		set;
 	}
 }
 public enum NinjaForce
