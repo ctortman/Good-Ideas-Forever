ddfb2fb6bc9b7cde5d001e2686b88023c513a176
andrusch <charles.h.andrus@gmail.com>
2014-04-12 09:57:57 -0500

    Initial game state objects

--- Good-Ideas-Forever/Assets/GameState.cs
+++ Good-Ideas-Forever/Assets/GameState.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameStae : MonoBehaviour {
+public class GameState : MonoBehaviour {
 
+	private Object[,] _gameboard;
+
+		
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		this._gameboard = new Object[12,12];
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 	
 	}
+
 }
