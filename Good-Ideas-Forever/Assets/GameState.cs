41dfa57f36fa9d6cbb7f089f80b6824e90abd010
andrusch <charles.h.andrus@gmail.com>
2014-04-12 10:07:01 -0500

    Basic Gameboard

--- Good-Ideas-Forever/Assets/GameState.cs
+++ Good-Ideas-Forever/Assets/GameState.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameState : MonoBehaviour {
 
 	private Object[,] _gameboard;
+	private List<EnemyShip> _rightShips;
+	private List<EnemyShip> _leftShips;
+	private PlayerShip _player;
 
-		
 	// Use this for initialization
 	void Start () 
 	{
