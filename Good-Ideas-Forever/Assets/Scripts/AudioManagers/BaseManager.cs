c8f711cb79db44356e9cc06c6175f8f0a156689a
andrusch <charles.h.andrus@gmail.com>
2014-04-13 08:56:45 -0500

    Fixed button hiding.

--- Good-Ideas-Forever/Assets/Scripts/AudioManagers/BaseManager.cs
+++ Good-Ideas-Forever/Assets/Scripts/AudioManagers/BaseManager.cs
@@ -24,6 +24,7 @@ public class BaseManager : MonoBehaviour
 	public AudioClip sfx2;
 	public AudioClip sfx3;
 	//public Texture2D backgroundSplash;
+	private bool displayButtons = true;
 
 
 	// declare instance
@@ -38,33 +39,39 @@ public class BaseManager : MonoBehaviour
 	// demonstration buttons for switching scenes
 	void OnGUI()
 	{
-		// display an area for buttons
-		//75,75,400,400,     default 20,20,600,600
-		GUILayout.BeginArea (new Rect (0, 0, Screen.width, Screen.height));
-		//GUI.BeginGroup (new Rect (0, 0, 300, 198));
-		//GUI.Box (new Rect (0, 0, 300, 198), backgroundSplash);
-		//GUILayout.Label ("Load a scene and fade in a looped Audio Clip");
-		GUILayout.Space (Screen.height*2/3);
-		//if (GUILayout.Button ("Menu Scene", GUILayout.Height (50))) { Application.LoadLevel 
-		//    (1); }
-		if (GUILayout.Button ("Game Scene", GUILayout.Height (Screen.height/10))) { Application.LoadLevel 
-			(2); }
-		//if (GUILayout.Button ("Title", GUILayout.Height (50))) {Application.LoadLevel 
-		//	(0); }
-		//if (GUILayout.Button ("End Scene", GUILayout.Height (60))) { Application.LoadLevel 
-		//	(4); }
-		GUILayout.Space (Screen.height/20);
-		// title
-		//GUILayout.Label ("Play a single sound");
-		GUILayout.BeginHorizontal ();
-		// Demonstrates playing a single sound clip
-		if (GUILayout.Button ("Play SFX 1")) { AudioHelper.CreatePlayAudioObject 
-			(sfx1); }
-		if (GUILayout.Button ("Play SFX 2")) { AudioHelper.CreatePlayAudioObject 
-			(sfx2); }
-		if (GUILayout.Button ("Play SFX 3")) { AudioHelper.CreatePlayAudioObject 
-			(sfx3); }
-		GUILayout.EndHorizontal ();
-		GUILayout.EndArea ();
+		if (this.displayButtons)
+		{
+			// display an area for buttons
+			//75,75,400,400,     default 20,20,600,600
+			GUILayout.BeginArea (new Rect (0, 0, Screen.width, Screen.height));
+			//GUI.BeginGroup (new Rect (0, 0, 300, 198));
+			//GUI.Box (new Rect (0, 0, 300, 198), backgroundSplash);
+			//GUILayout.Label ("Load a scene and fade in a looped Audio Clip");
+			GUILayout.Space (Screen.height*2/3);
+			//if (GUILayout.Button ("Menu Scene", GUILayout.Height (50))) { Application.LoadLevel 
+			//    (1); }
+			if (GUILayout.Button ("Play R.O.W.B.O.A.T.", GUILayout.Height (Screen.height/10))) 
+			{ Application.LoadLevel 
+				(2); 
+				this.displayButtons = false;
+			}
+			//if (GUILayout.Button ("Title", GUILayout.Height (50))) {Application.LoadLevel 
+			//	(0); }
+			//if (GUILayout.Button ("End Scene", GUILayout.Height (60))) { Application.LoadLevel 
+			//	(4); }
+			GUILayout.Space (Screen.height/20);
+			// title
+			//GUILayout.Label ("Play a single sound");
+			GUILayout.BeginHorizontal ();
+			// Demonstrates playing a single sound clip
+			if (GUILayout.Button ("Play SFX 1")) { AudioHelper.CreatePlayAudioObject 
+				(sfx1); }
+			if (GUILayout.Button ("Play SFX 2")) { AudioHelper.CreatePlayAudioObject 
+				(sfx2); }
+			if (GUILayout.Button ("Play SFX 3")) { AudioHelper.CreatePlayAudioObject 
+				(sfx3); }
+			GUILayout.EndHorizontal ();
+			GUILayout.EndArea ();
+		}
 	}
 }
